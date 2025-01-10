11dbf1a7125c38097823b29042bee4f0bb46f476
Eddie <eddie75espinoza@gmail.com>
2025-01-10 17:30:28 -0300

    refactor middleware

--- {{cookiecutter.project_name}}/backend/core/middleware.py
+++ {{cookiecutter.project_name}}/backend/core/middleware.py
@@ -1,44 +1,34 @@
 import jwt
 from fastapi import Request, HTTPException
-from functools import wraps
-from config import APP_CONFIG
+from core.config import APP_CONFIG
 
 
-def require_bearer_token(func):
-    @wraps(func)
-    async def decorated(request: Request, *args, **kwargs):
-        auth_header = request.headers.get('Authorization')
+def require_bearer_token(request: Request):
+    auth_header = request.headers.get('Authorization')
 
-        if not auth_header:
-            raise HTTPException(status_code=401, detail="Authorization header is missing!")
+    if not auth_header:
+        raise HTTPException(status_code=401, detail="Authorization header is missing!")
 
-        if not auth_header.startswith("Bearer "):
-            raise HTTPException(status_code=401, detail="Invalid Authorization header format!")
+    if not auth_header.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Invalid Authorization header format!")
 
-        token = auth_header.split(" ")[1]
+    token = auth_header.split(" ")[1]
 
-        try:
-            decoded_token = jwt.decode(
-                token, 
-                APP_CONFIG.TOKEN_SECRET_KEY, 
-                algorithms=["HS256"]
-            )
+    try:
+        decoded_token = jwt.decode(
+            token,
+            APP_CONFIG.TOKEN_SECRET_KEY,
+            algorithms=["HS256"]
+        )
 
-            if decoded_token.get("sub") != APP_CONFIG.SUB:
-                raise HTTPException(status_code=403, detail="Invalid token data")
+        if decoded_token.get("sub") != APP_CONFIG.SUB:
+            raise HTTPException(status_code=403, detail="Invalid token data")
 
-            # Validar cualquier otro campo relevante en el token
-            if decoded_token.get("iss") != "your-issuer-name":
-                raise HTTPException(status_code=403, detail="Invalid token issuer")
+    except jwt.exceptions.ExpiredSignatureError:  # Cambiado a 'exceptions.ExpiredSignatureError'
+        raise HTTPException(status_code=401, detail="Token has expired")
 
-        except jwt.ExpiredSignatureError as jwt_exc_exp:
-            raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.exceptions.InvalidTokenError:  # Cambiado a 'exceptions.InvalidTokenError'
+        raise HTTPException(status_code=403, detail="Invalid token")
 
-        except jwt.InvalidTokenError as jwt_exc_invalid:
-            raise HTTPException(status_code=403, detail="Invalid token")
-
-        except Exception as exc:
-            raise HTTPException(status_code=500, detail=f"An error occurred: {str(exc)}")
-
-        return await func(request, *args, **kwargs)
-    return decorated
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(exc)}")
