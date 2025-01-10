37bdeb01ee237804e4d77c77af876eea26552098
Eddie <eddie75espinoza@gmail.com>
2025-01-10 16:23:52 -0300

    refactor hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,15 +1,18 @@
+import os
 import secrets
 
-
 def main():
-    secret_key = secrets.token_urlsafe(32)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    template_root = os.path.join(current_dir, "..")
+    cookiecutter_json_path = os.path.join(template_root, "cookiecutter.json")
 
-    with open("cookiecutter.json", "r") as f:
+    with open(cookiecutter_json_path, "r") as f:
         content = f.read()
 
+    secret_key = secrets.token_urlsafe(32)
     content = content.replace("GENERATE_TOKEN_SECRET_KEY", secret_key)
 
-    with open("cookiecutter.json", "w") as f:
+    with open(cookiecutter_json_path, "w") as f:
         f.write(content)
 
 if __name__ == "__main__":
